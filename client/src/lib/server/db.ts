632c7322cd909278627a745c9ca04d3b880872ad
VeQox <sonnleitner.luki@gmail.com>
2023-06-24 13:03:21 +0200

    feat: add check constaints to ddl stmts

--- client/src/lib/server/db.ts
+++ client/src/lib/server/db.ts
@@ -19,21 +19,23 @@ export async function createConnection(): Promise<Database> {
 
 export const ensureTablesExist = async (connection: Database) => {
 	if (!(await tableExists(connection, "user"))) {
-		await connection.exec(`
+		Logger.info("User table does not exist");
+		await connection.exec(` 
 		CREATE TABLE IF NOT EXISTS user (
-			username TEXT PRIMARY KEY NOT NULL,
-			password TEXT NOT NULL,
+			username TEXT PRIMARY KEY NOT NULL CHECK(length(username) > 0 AND length(username) < 256),
+			password TEXT NOT NULL CHECK(length(password) = 64),
 			created_at INTEGER NOT NULL
 		);`);
 		if (await tableExists(connection, "user")) Logger.info("User table created");
 	}
 	if (!(await tableExists(connection, "token"))) {
+		Logger.info("Token table does not exist");
 		await connection.exec(`
 		CREATE TABLE IF NOT EXISTS token (
-			user TEXT NOT NULL,
-			session_token TEXT NOT NULL,
-			created_at INTEGER NOT NULL,
-			expires_at INTEGER NOT NULL,
+			user TEXT NOT NULL CHECK(length(user) > 0 AND length(user) < 256),
+			session_token TEXT NOT NULL CHECK(length(session_token) = 88),
+			created_at INTEGER NOT NULL CHECK(created_at > 0),
+			expires_at INTEGER NOT NULL CHECK(expires_at > created_at),
 			FOREIGN KEY (user) REFERENCES user(username),
 			PRIMARY KEY (session_token)
 		);`);

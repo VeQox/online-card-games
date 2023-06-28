66c668c3d270fdb81eaf7df286d37a6ed8f78a33
VeQox <sonnleitner.luki@gmail.com>
2023-06-28 09:48:42 +0200

    ui: add layout (unfinished)

--- server/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { Logger } from "@logger";
-import { Server, createServer } from "https";
-
-const port: number = 3000;
-const server: Server = createServer();
-server.listen(port, async () => {
-	console.log(`Server listening on port ${port}`);
-});
